adb4b69034324e373d478b2f1d412eb5ef4a9855
MARCROCK22 <marcos22dev@gmail.com>
2024-08-17 16:57:54 +0000

    watcher package

--- packages/uws-adapter/src/adapter.ts
+++ packages/uws-adapter/src/adapter.ts
@@ -1,116 +1,110 @@
-import { HttpClient, Logger, } from 'seyfert';
-import { InternalRuntimeConfigHTTP } from 'seyfert/lib/client/base';
-import { APIInteraction, InteractionResponseType, InteractionType } from 'seyfert/lib/types';
+import type { HttpClient, Logger } from 'seyfert';
+import type { InternalRuntimeConfigHTTP } from 'seyfert/lib/client/base';
+import { type APIInteraction, InteractionResponseType, InteractionType } from 'seyfert/lib/types';
 import type { HttpServerAdapter } from 'seyfert/lib/client/types';
 
 import nacl from 'tweetnacl';
-import { App, HttpRequest, HttpResponse, type TemplatedApp } from 'uWebSockets.js'
+import { App, type HttpRequest, type HttpResponse, type TemplatedApp } from 'uWebSockets.js';
 
 export class UwsAdapter implements HttpServerAdapter {
-    public app!: TemplatedApp
-    publicKeyHex!: Buffer;
-    applicationId!: string;
-    debugger?: Logger;
-    logger!: Logger
+	public app!: TemplatedApp;
+	publicKeyHex!: Buffer;
+	applicationId!: string;
+	debugger?: Logger;
+	logger: Logger;
 
-    constructor(public client: HttpClient) {
-        this.logger = client.logger
-    }
+	constructor(public client: HttpClient) {
+		this.logger = client.logger;
+	}
 
-    async start(path: `/${string}` = "/interactions", uwsApp?: TemplatedApp) {
-        if (this.client.debugger) this.debugger = this.client.debugger
+	async start(path: `/${string}` = '/interactions', uwsApp?: TemplatedApp) {
+		if (this.client.debugger) this.debugger = this.client.debugger;
 
-        const {
-            publicKey,
-            port,
-            applicationId,
-        } = await this.client.getRC<InternalRuntimeConfigHTTP>()
+		const { publicKey, port, applicationId } = await this.client.getRC<InternalRuntimeConfigHTTP>();
 
-        if (!publicKey) {
-            throw new Error('Expected a publicKey, check your config file');
-        }
-        if (!port && !uwsApp) {
-            throw new Error('Expected a port, check your config file');
-        }
-        if (applicationId) {
-            this.applicationId = applicationId;
-        }
+		if (!publicKey) {
+			throw new Error('Expected a publicKey, check your config file');
+		}
+		if (!(port || uwsApp)) {
+			throw new Error('Expected a port, check your config file');
+		}
+		if (applicationId) {
+			this.applicationId = applicationId;
+		}
 
-        this.publicKeyHex = Buffer.from(publicKey, 'hex');
-        this.app = uwsApp ?? App()
-        this.app.post(path, this.onPacket.bind(this));
+		this.publicKeyHex = Buffer.from(publicKey, 'hex');
+		this.app = uwsApp ?? App();
+		this.app.post(path, this.onPacket.bind(this));
 
-        if (!uwsApp) {
-            this.app.listen(port, () => {
-                this.logger.info(`Listening to <url>:${port}${path}`);
-            });
-        } else {
-            this.logger.info(`Running on <url>${path}`);
-        }
-    }
+		if (uwsApp) {
+			this.logger.info(`Running on <url>${path}`);
+		} else {
+			this.app.listen(port, () => {
+				this.logger.info(`Listening to <url>:${port}${path}`);
+			});
+		}
+	}
 
-    protected async verifySignature(res: HttpResponse, req: HttpRequest) {
-        const timestamp = req.getHeader('x-signature-timestamp');
-        const ed25519 = req.getHeader('x-signature-ed25519');
-        const body = await UwsAdapter.readJson<APIInteraction>(res);
-        if (
-            nacl.sign.detached.verify(
-                Buffer.from(timestamp + JSON.stringify(body)),
-                Buffer.from(ed25519, 'hex'),
-                this.publicKeyHex,
-            )
-        ) {
-            return body;
-        }
-        return;
-    }
+	protected async verifySignature(res: HttpResponse, req: HttpRequest) {
+		const timestamp = req.getHeader('x-signature-timestamp');
+		const ed25519 = req.getHeader('x-signature-ed25519');
+		const body = await UwsAdapter.readJson<APIInteraction>(res);
+		if (
+			nacl.sign.detached.verify(
+				Buffer.from(timestamp + JSON.stringify(body)),
+				Buffer.from(ed25519, 'hex'),
+				this.publicKeyHex,
+			)
+		) {
+			return body;
+		}
+		return;
+	}
 
-    protected async onPacket(res: HttpResponse, req: HttpRequest) {
-        const rawBody = await this.verifySignature(res, req);
-        if (!rawBody) {
-            this.debugger?.debug('Invalid request/No info, returning 418 status.');
-            // I'm a teapot
-            res.writeStatus('418').end();
-            return
-        }
-        switch (rawBody.type) {
-            case InteractionType.Ping:
-                this.debugger?.debug('Ping interaction received, responding.');
-                res
-                    .writeHeader('Content-Type', 'application/json')
-                    .end(JSON.stringify({ type: InteractionResponseType.Pong }));
-                break;
-            default:
-                res.cork(async () => {
-                    const { headers, response } = await this.client.onPacket(rawBody)
-                    for (const i in headers) {
-                        res.writeHeader(i, headers[i as keyof typeof headers]!);
-                    }
-                    res.end(JSON.stringify(response))
-                });
-                return;
-        }
-    }
+	protected async onPacket(res: HttpResponse, req: HttpRequest) {
+		const rawBody = await this.verifySignature(res, req);
+		if (!rawBody) {
+			this.debugger?.debug('Invalid request/No info, returning 418 status.');
+			// I'm a teapot
+			res.writeStatus('418').end();
+			return;
+		}
+		switch (rawBody.type) {
+			case InteractionType.Ping:
+				this.debugger?.debug('Ping interaction received, responding.');
+				res.writeHeader('Content-Type', 'application/json').end(JSON.stringify({ type: InteractionResponseType.Pong }));
+				break;
+			default:
+				res.cork(async () => {
+					const { headers, response } = await this.client.onPacket(rawBody);
+					for (const i in headers) {
+						res.writeHeader(i, headers[i as keyof typeof headers]!);
+					}
+					res.end(JSON.stringify(response));
+				});
+				return;
+		}
+	}
 
-    protected static readJson<T extends Record<string, any>>(res: HttpResponse) {
-        return new Promise<T>((ok, rej) => {
-            const buffers: Buffer[] = [];
-            res.onData((ab, isLast) => {
-                const chunk = Buffer.from(ab);
-                if (isLast) {
-                    try {
-                        buffers.push(chunk)
-                        ok(JSON.parse(Buffer.concat(buffers).toString()));
-                    } catch (e) {
-                        res.close();
-                        return;
-                    }
-                } else {
-                    buffers.push(chunk)
-                }
-            });
+	protected static readJson<T extends Record<string, any>>(res: HttpResponse) {
+		return new Promise<T>((ok, rej) => {
+			const buffers: Buffer[] = [];
+			res.onData((ab, isLast) => {
+				const chunk = Buffer.from(ab);
+				if (isLast) {
+					try {
+						buffers.push(chunk);
+						ok(JSON.parse(Buffer.concat(buffers).toString()));
+					} catch (e) {
+						res.close();
+						return;
+					}
+				} else {
+					buffers.push(chunk);
+				}
+			});
 
-            res.onAborted(rej);
-        });
-    }
+			res.onAborted(rej);
+		});
+	}
 }
---
+++ packages/watcher/src/watcher.ts
@@ -0,0 +1,149 @@
+import { execSync } from 'node:child_process';
+import { ShardManager, Logger, ApiHandler, Router } from 'seyfert';
+import { BaseClient, type InternalRuntimeConfig } from 'seyfert/lib/client/base';
+import type { MakeRequired, MakePartial } from 'seyfert/lib/common';
+import type { GatewayDispatchPayload, GatewaySendPayload } from 'seyfert/lib/types';
+import type { ShardManagerDefaults, ShardManagerOptions } from 'seyfert/lib/websocket';
+import { Worker } from 'node:worker_threads';
+import { watch } from 'chokidar';
+
+/**
+ * Represents a watcher class that extends the ShardManager.
+ */
+export class Watcher extends ShardManager {
+	worker?: import('node:worker_threads').Worker;
+	logger = new Logger({
+		name: '[Watcher]',
+	});
+	rest?: ApiHandler;
+
+	declare options: MakeRequired<WatcherOptions, 'token' | 'info' | keyof typeof ShardManagerDefaults>;
+
+	/**
+	 * Initializes a new instance of the Watcher class.
+	 * @param options The options for the watcher.
+	 */
+	constructor(options: WatcherOptions) {
+		super({
+			handlePayload() {},
+			token: '',
+			intents: 0,
+			info: {
+				url: 'wss://gateway.discord.gg',
+				session_start_limit: {
+					max_concurrency: -1,
+					remaining: -1,
+					reset_after: -1,
+					total: -1,
+				},
+				shards: -1,
+			},
+			...options,
+		});
+	}
+
+	/**
+	 * Resets the worker instance.
+	 */
+	resetWorker() {
+		if (this.worker) {
+			this.worker.terminate();
+		}
+		this.build();
+		this.worker = new Worker(this.options.filePath, {
+			argv: this.options.argv,
+			workerData: {
+				__USING_WATCHER__: true,
+			},
+		});
+		this.worker!.on('message', (data: WatcherSendToShard) => {
+			switch (data.type) {
+				case 'SEND_TO_SHARD':
+					this.send(data.shardId, data.payload);
+					break;
+			}
+		});
+	}
+
+	/**
+	 * Spawns shards for the watcher.
+	 */
+	async spawnShards() {
+		const RC = await BaseClient.prototype.getRC<InternalRuntimeConfig>();
+		this.options.token = RC.token;
+		this.rest ??= new ApiHandler({
+			baseUrl: 'api/v10',
+			domain: 'https://discord.com',
+			token: this.options.token,
+		});
+		this.options.intents = RC.intents;
+		this.options.info = await new Router(this.rest!).createProxy().gateway.bot.get();
+		this.options.totalShards = this.options.info.shards;
+
+		this.resetWorker();
+
+		const oldFn = this.options.handlePayload;
+		this.options.handlePayload = (shardId, payload) => {
+			this.worker?.postMessage({
+				type: 'PAYLOAD',
+				shardId,
+				payload,
+			} satisfies WatcherPayload);
+			return oldFn?.(shardId, payload);
+		};
+		this.connectQueue.concurrency = this.options.info.session_start_limit.max_concurrency;
+
+		await super.spawnShards();
+
+		const watcher = watch(this.options.srcPath).on('ready', () => {
+			this.logger.debug(`Watching ${this.options.srcPath}`);
+			watcher.on('all', event => {
+				this.logger.debug(`${event} event detected, building`);
+				this.resetWorker();
+			});
+		});
+	}
+
+	/**
+	 * Builds the watcher.
+	 */
+	protected build() {
+		if (this.options.transpileCommand) execSync(`cd ${process.cwd()} && ${this.options.transpileCommand}`);
+		this.logger.info('Builded');
+	}
+}
+
+export interface WatcherOptions
+	extends MakePartial<
+		Omit<ShardManager['options'], 'handlePayload' | 'info' | 'token' | 'intents'>,
+		| 'compress'
+		| 'presence'
+		| 'properties'
+		| 'shardEnd'
+		| 'shardStart'
+		| 'spawnShardDelay'
+		| 'totalShards'
+		| 'url'
+		| 'version'
+	> {
+	filePath: string;
+	transpileCommand?: string;
+	srcPath: string;
+	argv?: string[];
+	handlePayload?: ShardManagerOptions['handlePayload'];
+	info?: ShardManagerOptions['info'];
+	token?: ShardManagerOptions['token'];
+	intents?: ShardManagerOptions['intents'];
+}
+
+export interface WatcherPayload {
+	type: 'PAYLOAD';
+	shardId: number;
+	payload: GatewayDispatchPayload;
+}
+
+export interface WatcherSendToShard {
+	type: 'SEND_TO_SHARD';
+	shardId: number;
+	payload: GatewaySendPayload;
+}
