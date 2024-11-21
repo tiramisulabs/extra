5472c6bf9b3a4953cf5c915785b717beea152e03
MARCROCK22 <marcos22dev@gmail.com>
2024-11-21 15:43:36 -0400

    fix: FormData body

--- packages/uws-adapter/src/adapter.ts
+++ packages/uws-adapter/src/adapter.ts
@@ -1,9 +1,9 @@
 import type { Logger, UsingClient } from 'seyfert';
 import type { HttpServerAdapter } from 'seyfert/lib/client/types';
 import { type APIInteraction, InteractionResponseType, InteractionType } from 'seyfert/lib/types';
-
 import nacl from 'tweetnacl';
 import { App, type HttpRequest, type HttpResponse, type TemplatedApp } from 'uWebSockets.js';
+import type { File } from '../../../node_modules/.pnpm/undici-types@6.19.8/node_modules/undici-types/file';
 
 export class UwsAdapter implements HttpServerAdapter {
 	public app!: TemplatedApp;
@@ -80,7 +80,41 @@ export class UwsAdapter implements HttpServerAdapter {
 					for (const i in headers) {
 						res.writeHeader(i, headers[i as keyof typeof headers]!);
 					}
-					res.end(JSON.stringify(response));
+					if (response instanceof FormData) {
+						const files: File[] = [];
+						let body: string | undefined;
+						for (const [key, value] of response.entries()) {
+							if (key === 'payload_json') {
+								body = value as string;
+							} else {
+								files.push(value as File);
+							}
+						}
+
+						const boundary = `${Date.now() + Math.random().toString(10).slice(2)}`;
+						res.cork(async () => {
+							res.writeHeader('Content-Type', `multipart/form-data; boundary=${boundary}`);
+							if (body) {
+								res.write(
+									`\r\n--${boundary}\r\nContent-Disposition: form-data; name="payload_json"\r\nContent-Type: application/json\r\n\r\n${body}`,
+								);
+							}
+							if (files.length) {
+								for (let i = 0; i < files.length; i++) {
+									const file = files[i];
+
+									const bytes = await file.bytes();
+									res.cork(() => {
+										res.write(
+											`\r\n--${boundary}\r\nContent-Disposition: form-data; name="files[${i}]"; filename="${file.name}"${file.type ? `\r\nContent-Type: ${file.type}` : ''}\r\n\r\n`,
+										);
+										res.write(bytes);
+									});
+								}
+							}
+							res.cork(() => res.end(`\r\n--${boundary}--`));
+						});
+					} else res.end(JSON.stringify(response));
 				});
 				break;
 		}
